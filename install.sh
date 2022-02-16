456c28f9b01c5193fab999a18e3b6877418d47e0
paul <paul@noemail.net>
2022-02-16 13:26:00 +0000

    added sndkit to install script
    FossilOrigin-Name: 3fd16d055eb68b5cbddae0502c3c6a778d991212299930403441405df4aced56

--- install.sh
+++ install.sh
@@ -23,5 +23,17 @@ cp $MNOLTH_PATH/core/scheme.h $INSTALL_PATH/include/mnolth/scheme
 mkdir -p $INSTALL_PATH/include/mnolth/scheme
 cp $MNOLTH_PATH/core/hotswap.h $INSTALL_PATH/include/mnolth/
 
+mkdir -p $INSTALL_PATH/include/mnolth/sndkit
+mkdir -p $INSTALL_PATH/include/mnolth/sndkit/nodes
+mkdir -p $INSTALL_PATH/include/mnolth/sndkit/lil
+mkdir -p $INSTALL_PATH/include/mnolth/sndkit/dsp
+mkdir -p $INSTALL_PATH/include/mnolth/sndkit/graforge
+cp $LIBPATH/sndkit/core.h $INSTALL_PATH/include/mnolth/sndkit
+cp $LIBPATH/sndkit/nodes/sknodes.h $INSTALL_PATH/include/mnolth/sndkit/nodes
+cp $LIBPATH/sndkit/nodes/sklil.h $INSTALL_PATH/include/mnolth/sndkit/nodes
+cp $LIBPATH/sndkit/dsp/*.h $INSTALL_PATH/include/mnolth/sndkit/dsp
+cp $LIBPATH/sndkit/lil/lil.h $INSTALL_PATH/include/mnolth/sndkit/lil
+cp $LIBPATH/sndkit/graforge/graforge.h $INSTALL_PATH/include/mnolth/sndkit/graforge
+
 mkdir -p $INSTALL_PATH/share/mnolth
 cp $MNOLTH_PATH/scm/*.scm $INSTALL_PATH/share/mnolth
