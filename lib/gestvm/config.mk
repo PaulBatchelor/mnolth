82704fbe61cdd98ef4df34d7f31fb5dc1f6da94b
paul <>
2022-08-21 21:15:15 +0000

    memops: more boilerplate. some generated code compiles
    FossilOrigin-Name: 58ff91be467d8a15e58615afb06e98b076a8c2752957c17d95519cc2706fb68e

--- lib/gestvm/config.mk
+++ lib/gestvm/config.mk
@@ -3,4 +3,8 @@ OBJ +=  lib/gestvm/gestvm.o \
 	lib/gestvm/uxn/uxn.o \
 	lib/gestvm/uxn/uxnasm.o
 
+# Extra objects (not part of standard gestvm)
+
+OBJ += lib/gestvm/memops.o
+
 CFLAGS += -DNO_UXNASM_MAIN
