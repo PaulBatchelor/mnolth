150199fd806efd0d93fe48abcbf4adb608397bc8
paul <>
2022-10-23 10:30:38 +0000

    added wavdraw utility
    FossilOrigin-Name: a3c143e7f86c7f31a07d08b15b6f7debd7cad6342428a4a7e7b8399f345f3a93

--- lib/btprnt/config.mk
+++ lib/btprnt/config.mk
@@ -4,8 +4,10 @@ OBJ+= lib/btprnt/regpool.o
 OBJ+= lib/btprnt/pngencode.o
 OBJ+= lib/btprnt/base64.o
 OBJ+= lib/btprnt/macfont.o
+OBJ+= lib/btprnt/wavdraw.o
 
 TANGLED+= lib/btprnt/macfont.c
 TANGLED+= lib/btprnt/pngencode.c
 TANGLED+= lib/btprnt/regpool.c
 TANGLED+= lib/btprnt/regpool.h
+TANGLED+= lib/btprnt/wavdraw.c
