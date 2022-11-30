db34f3896db86a9e5d6d2802a7709e8b11666c05
paul <>
2022-11-30 11:43:58 +0000

    updated snkdit
    FossilOrigin-Name: 2c1ebb546b86a60fcc87379d1f3fa37e8870048952cac8def2570fdd046a8de5

--- lib/sndkit/dsp/butterworth.c
+++ lib/sndkit/dsp/butterworth.c
@@ -1,4 +1,4 @@
-#line 58 "butterworth.org"
+#line 60 "butterworth.org"
 #include <math.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -11,7 +11,7 @@
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
-
+#line 94 "butterworth.org"
 static SKFLT filter(SKFLT in, SKFLT *a)
 {
     SKFLT t, y;
@@ -27,7 +27,9 @@ void sk_butterworth_init(sk_butterworth *bw, int sr)
 {
     int i;
     sk_butterworth_freq(bw, 1000.0);
+    sk_butterworth_bandwidth(bw, 1000.0);
     bw->lfreq = -1;
+    bw->lbw = -1;
     bw->pidsr = M_PI / (SKFLT)sr;
     for (i = 0; i < 7; i++) bw->a[i] = 0.0;
 }
@@ -37,6 +39,12 @@ void sk_butterworth_freq(sk_butterworth *bw, SKFLT freq)
     bw->freq = freq;
 }
 
+void sk_butterworth_bandwidth(sk_butterworth *but, SKFLT bw)
+{
+    but->bw = bw;
+}
+#line 60 "butterworth.org"
+#line 130 "butterworth.org"
 SKFLT sk_butlp_tick(sk_butterworth *bw, SKFLT in)
 {
     if (bw->freq != bw->lfreq) {
@@ -56,7 +64,7 @@ SKFLT sk_butlp_tick(sk_butterworth *bw, SKFLT in)
 
     return filter(in, bw->a);
 }
-
+#line 153 "butterworth.org"
 SKFLT sk_buthp_tick(sk_butterworth *bw, SKFLT in)
 {
     if (bw->freq != bw->lfreq) {
@@ -76,3 +84,25 @@ SKFLT sk_buthp_tick(sk_butterworth *bw, SKFLT in)
 
     return filter(in, bw->a);
 }
+#line 176 "butterworth.org"
+SKFLT sk_butbp_tick(sk_butterworth *bw, SKFLT in)
+{
+    if (bw->bw != bw->lbw || bw->freq != bw->lfreq) {
+        SKFLT *a, c, d;
+        a = bw->a;
+        bw->lfreq = bw->freq;
+        bw->lbw = bw->bw;
+
+        /* Perform BLT and store components */
+        c = 1.0 / tan((SKFLT)(bw->pidsr * bw->bw));
+        d = 2.0 * cos((SKFLT)(2.0*bw->pidsr * bw->freq));
+        a[0] = 1.0 / (1.0 + c);
+        a[1] = 0.0;
+        a[2] = -a[0];
+        a[3] = - c * d * a[0];
+        a[4] = (c - 1.0) * a[0];
+    }
+
+    return filter(in, bw->a);
+}
+#line 60 "butterworth.org"
--- lib/sndkit/dsp/butterworth.h
+++ lib/sndkit/dsp/butterworth.h
@@ -10,15 +10,18 @@ typedef struct sk_butterworth sk_butterworth;
 
 void sk_butterworth_init(sk_butterworth *bw, int sr);
 void sk_butterworth_freq(sk_butterworth *bw, SKFLT freq);
+void sk_butterworth_bandwidth(sk_butterworth *but, SKFLT bw);
 
 SKFLT sk_butlp_tick(sk_butterworth *bw, SKFLT in);
 SKFLT sk_buthp_tick(sk_butterworth *bw, SKFLT in);
+SKFLT sk_butbp_tick(sk_butterworth *bw, SKFLT in);
 
 #ifdef SK_BUTTERWORTH_PRIV
 struct sk_butterworth {
     SKFLT freq, lfreq;
+    SKFLT bw, lbw; /* for bandpass filter only */
     SKFLT a[7];
-    SKFLT pidsr;
+    SKFLT pidsr, tpidsr;
 };
 #endif
 
