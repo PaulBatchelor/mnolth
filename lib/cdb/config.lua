027821c606a497562ebd58ff7ae487bd50030d78
paul <>
2022-12-24 02:25:09 +0000

    added sqlite3 config
    FossilOrigin-Name: 2bd2c315e391225d7564d4ad5cf7bb99c1a4384e43ae07d3e4aeadc2d5e4af75

---
+++ lib/cdb/config.lua
@@ -0,0 +1,13 @@
+add_objects {
+    "lib/cdb/cdb",
+    "lib/cdb/find",
+    "lib/cdb/findnext",
+    "lib/cdb/hash",
+    "lib/cdb/init",
+    "lib/cdb/make",
+    "lib/cdb/make_add",
+    "lib/cdb/make_put",
+    "lib/cdb/seek",
+    "lib/cdb/seq",
+    "lib/cdb/unpack",
+}
