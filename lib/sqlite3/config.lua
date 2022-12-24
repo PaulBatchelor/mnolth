a5a7abc6dce6fb2fc99c80fdd2b02ab29316c911
paul <paul@noemail.net>
2022-12-24 02:25:09 +0000

    added sqlite3 config
    FossilOrigin-Name: 2bd2c315e391225d7564d4ad5cf7bb99c1a4384e43ae07d3e4aeadc2d5e4af75

---
+++ lib/sqlite3/config.lua
@@ -0,0 +1,24 @@
+add_c99object("lib/sqlite3/sqlite3")
+add_c99object("lib/sqlite3/shell")
+
+add_cflags {
+    "-DSQLITE_ENABLE_FTS5 -DSQLITE_ENABLE_JSON1",
+
+    -- recommended SQLite build flags for CLI
+    "-DSQLITE_THREADSAFE=0",
+    "-DSQLITE_ENABLE_EXPLAIN_COMMENTS",
+    "-DSQLITE_INTROSPECTION_PRAGMAS",
+    "-DSQLITE_ENABLE_UNKNOWN_SQL_FUNCTION",
+    "-DSQLITE_ENABLE_STMTVTAB",
+    "-DSQLITE_ENABLE_DBPAGE_VTAB",
+    "-DSQLITE_ENABLE_DBSTAT_VTAB",
+    "-DSQLITE_ENABLE_OFFSET_SQL_FUNC",
+    "-DSQLITE_ENABLE_RTREE",
+    "-DSQLITE_ENABLE_FTS4"
+}
+
+if use_readline == true then
+    add_cflags {
+        "-DHAVE_READLINE=1"
+    }
+end
