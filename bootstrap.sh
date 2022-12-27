9415aed74b9a14cf5ee56cf728f1af836ab0ae92
paul <>
2022-12-27 15:50:19 +0000

    place bootstrap binaries in tools folder
    FossilOrigin-Name: cb5b11a6224554009f14fcf6b26f4ac057120bc6da3ea999969e08601d420cae

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 MNOLTH_TOP=$(pwd)
 
+mkdir -p tools
 echo "building lua"
-cd lib/lua && ./build.sh $MNOLTH_TOP/lua && cd $MNOLTH_TOP
+cd lib/lua && ./build.sh $MNOLTH_TOP/tools/lua && cd $MNOLTH_TOP
 echo "building samurai"
-cd util/samurai && ./build.sh $MNOLTH_TOP/samu
+cd util/samurai && ./build.sh $MNOLTH_TOP/tools/samu
