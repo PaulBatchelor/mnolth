ff013c1d3c71637431548363c5c804339f30b63a
paul <>
2023-03-31 14:05:11 +0000

    added heart shape
    FossilOrigin-Name: 4b84cb2b5ae693136ec20baa8002bbe49cdc8fc4f71ef044a1ffbc2ae4d31a2e

--- core/l_draw.c
+++ core/l_draw.c
@@ -96,8 +96,41 @@ static lil_value_t l_gfxcirc(lil_t lil,
     return NULL;
 }
 
+static lil_value_t l_gfxheart(lil_t lil,
+                              size_t argc,
+                              lil_value_t *argv)
+{
+    sk_core *core;
+    gfxbuf *gfx;
+    void *ptr;
+    int rc;
+    double cx, cy, r;
+    int pos;
+
+    SKLIL_ARITY_CHECK(lil, "gfxheart", argc, 4);
+
+    core = lil_get_data(lil);
+
+    rc = sk_core_generic_pop(core, &ptr);
+    SKLIL_ERROR_CHECK(lil, rc, "could not get gfxbuf instance.");
+
+    gfx = (gfxbuf *)ptr;
+
+    cx = lil_to_double(argv[0]);
+    cy = lil_to_double(argv[1]);
+    r = lil_to_double(argv[2]);
+    pos = lil_to_integer(argv[3]);
+
+    gfxdrw_heart(gfx, cx, cy, r, pos);
+
+    rc = sk_core_generic_push(core, ptr);
+    SKLIL_ERROR_CHECK(lil, rc, "could not push gfxbuf instance.");
+    return NULL;
+}
+
 void lil_load_draw(lil_t lil)
 {
     lil_register(lil, "gfxrectf", gfxrectf);
     lil_register(lil, "gfxcirc", l_gfxcirc);
+    lil_register(lil, "gfxheart", l_gfxheart);
 }
