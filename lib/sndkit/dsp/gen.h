2a0caf3303d5373a3006c3db0a0e7a5fdf8df3f7
paul <>
2023-10-19 19:35:36 +0000

    updated sndkit
    FossilOrigin-Name: 281f8cf67c1aec4d2be685c2e36b7b60288bfd9b1489261c4a1e69f9cca45049

--- lib/sndkit/dsp/gen.h
+++ lib/sndkit/dsp/gen.h
@@ -6,16 +6,18 @@
 #define SKFLT float
 #endif
 
-#line 50 "gen.org"
+#line 52 "gen.org"
 void sk_gen_sine(SKFLT *tab, unsigned long sz);
-#line 77 "gen.org"
+#line 79 "gen.org"
 void sk_gen_saw(SKFLT *tab, unsigned long sz);
-#line 105 "gen.org"
+#line 107 "gen.org"
 void sk_gen_vals(SKFLT **ptab, int *psz, const char *str);
-#line 174 "gen.org"
+#line 176 "gen.org"
 void sk_gen_sinesum(SKFLT *tab,
                     int sz,
                     const char *argstring,
                     int normalize);
+#line 234 "gen.org"
+int sk_gen_line(SKFLT *tab, int sz, const char *argstring);
 #line 27 "gen.org"
 #endif
