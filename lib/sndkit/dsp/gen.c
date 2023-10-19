19f4bdd62c4f2f62ea3b51231ea1669900faec57
paul <paul@noemail.net>
2023-10-19 19:35:36 +0000

    updated sndkit
    FossilOrigin-Name: 281f8cf67c1aec4d2be685c2e36b7b60288bfd9b1489261c4a1e69f9cca45049

--- lib/sndkit/dsp/gen.c
+++ lib/sndkit/dsp/gen.c
@@ -3,13 +3,15 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdint.h>
 #include "gen.h"
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-#line 55 "gen.org"
+#line 57 "gen.org"
 void sk_gen_sine(SKFLT *tab, unsigned long sz)
 {
     int i;
@@ -23,7 +25,7 @@ void sk_gen_sine(SKFLT *tab, unsigned long sz)
         tab[i] = sin(i * step);
     }
 }
-#line 82 "gen.org"
+#line 84 "gen.org"
 void sk_gen_saw(SKFLT *tab, unsigned long sz)
 {
     int i;
@@ -37,7 +39,7 @@ void sk_gen_saw(SKFLT *tab, unsigned long sz)
         tab[i] = 2.0*(i*step) - 1.0;
     }
 }
-#line 110 "gen.org"
+#line 112 "gen.org"
 static char * tokenize(char **next, int *size)
 {
     char *token;
@@ -98,7 +100,7 @@ void sk_gen_vals(SKFLT **ptab, int *psz, const char *string)
     *psz = sz;
     free(ptr);
 }
-#line 182 "gen.org"
+#line 184 "gen.org"
 void sk_gen_sinesum(SKFLT *tab,
                     int sz,
                     const char *argstring,
@@ -144,5 +146,65 @@ void sk_gen_sinesum(SKFLT *tab,
     }
 
     free(args);
+}
+#line 239 "gen.org"
+int sk_gen_line(SKFLT *tab, int sz, const char *argstring)
+{
+    uint16_t i, n, seglen;
+    SKFLT incr, amp = 0;
+    SKFLT x1, x2, y1, y2;
+    SKFLT *args;
+    int argsz;
+
+    args = malloc(sizeof(SKFLT));
+    args[0] = 0;
+    argsz = 1;
+
+    sk_gen_vals(&args, &argsz, argstring);
+
+    if ((argsz % 2) == 1 || argsz == 1) {
+        fprintf(stderr, "Error: not enough arguments for gen_line.\n");
+        free(args);
+        return 1;
+    } else if (argsz == 2) {
+        for (i = 0; i < sz; i++) {
+            tab[i] = args[1];
+        }
+        free(args);
+        return 0;
+    }
+
+    x1 = args[0];
+    y1 = args[1];
+    n = 0;
+    for (i = 2; i < argsz; i += 2) {
+        x2 = args[i];
+        y2 = args[i + 1];
+
+        if (x2 < x1) {
+            fprintf(stderr, "Error: x coordiates must be sequential!\n");
+            break;
+        }
+
+        seglen = (x2 - x1);
+        incr = (SKFLT)(y2 - y1) / (seglen - 1);
+        amp = y1;
+
+        while (seglen != 0) {
+            if (n < sz) {
+                tab[n] = amp;
+                amp += incr;
+                seglen--;
+                n++;
+            } else {
+                break;
+            }
+        }
+        y1 = y2;
+        x1 = x2;
+    }
+
+    free(args);
+    return 0;
 }
 #line 32 "gen.org"
