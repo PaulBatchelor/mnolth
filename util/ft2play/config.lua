2b6b221d9c91f19a8e68cfbc7e55534edcb3512b
paul <>
2024-08-04 22:21:08 +0000

    missing config
    FossilOrigin-Name: 7a01800acbf7a5841f7e9d7f4812b6ae16692a54bfcf153de375bae31560f3a9

---
+++ util/ft2play/config.lua
@@ -0,0 +1,16 @@
+-- mac hack
+-- add_cflags {
+--     "-DNO_GETLOADAVG"
+-- }
+
+add_c99objects {
+    "util/ft2play/dummydriver",
+    "util/ft2play/ft2play",
+    "util/ft2play/pmp_main",
+    "util/ft2play/pmp_mix",
+    "util/ft2play/pmplay",
+    "util/ft2play/snd_masm",
+    "util/ft2play/tables",
+}
+
+
